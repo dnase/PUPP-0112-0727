5dfa64c2e697306406c0c2bdff7cee4ec59f8e5a
drewbilee <drewbilee@puppetlabs.vm>
2017-01-25 19:27:58 +0000

    lab 10.2

--- modules/nginx/manifests/init.pp
+++ modules/nginx/manifests/init.pp
@@ -1,32 +1,53 @@
 class nginx {
+  case $::osfamily {
+    'redhat', 'debian' : {
+      $package = 'nginx'
+      $owner = 'root'
+      $group = 'root'
+      $docroot = '/var/www'
+      $confdir = '/etc/nginx'
+      $blockdir = '/etc/nginx/conf.d'
+    }
+    'windows' : {
+      $package = 'nginx-service'
+      $owner = 'Administrator'
+      $group = 'Administrators'
+      $docroot = 'C:/ProgramData/nginx/html'
+      $confdir = 'C:/ProgramData/nginx/conf'
+      $blockdir = 'C:/ProgramData/nginx/conf.d'
+    }
+    default : {
+      fail("Module ${module_name} is not supported on ${::osfamily}")
+    }
+  }
   File {
     owner => 'root',
     group => 'root',
     mode  => '0644',
   }
-  package { 'nginx':
+  package { $package:
     ensure => present,
   }
   file { 'webroot':
     ensure => directory,
-    path   => '/var/www',
+    path   => $docroot,
   }
   file { 'index.html':
     ensure => file,
-    path   => '/var/www/index.html',
-    source => 'puppet:///modules/nginx/index.html',
+    path   => "${docroot}/index.html",
+    source => "puppet:///modules/nginx/index.html",
   }
   file { 'nginx.conf':
     ensure  => file,
-    path    => '/etc/nginx/nginx.conf',
-    source  => 'puppet:///modules/nginx/nginx.conf',
-    require => Package['nginx'],
+    path    => "${confdir}/nginx.conf",
+    source  => "puppet:///modules/nginx/${::osfamily}.conf",
+    require => Package[$package],
   }
   file { 'default.conf':
     ensure  => file,
-    path    => '/etc/nginx/conf.d/default.conf',
-    source  => 'puppet:///modules/nginx/default.conf',
-    require => Package['nginx'],
+    path    => "${blockdir}/default.conf",
+    source  => "puppet:///modules/nginx/default-${::kernel}.conf",
+    require => Package[$package],
   }
   service { 'nginx':
     ensure    => running,
